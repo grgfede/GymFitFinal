c89cecdb397ea752a0a9d5d745ab8b36e8f79289
federico <federico.frari1995@gmail.com>
2020-06-17 11:11:03 +0200

    Aggiunta profiloPalestra

---
+++ GymFitFinal/GymFitFinal/home/palestra/profiloPalestra.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymFitFinal.home.palestra
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class profiloPalestra : ContentPage
+    {
+        public profiloPalestra()
+        {
+            InitializeComponent();
+        }
+    }
+}
