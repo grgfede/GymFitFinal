ded08cbfb9d5e5f1e0e0cd1fc44edeb87e6582f0
federico <federico.frari1995@gmail.com>
2020-09-23 11:28:27 +0200

    Fixed bot page

--- GymFitFinal/GymFitFinal/home/navBar/Palestra.xaml.cs
+++ GymFitFinal/GymFitFinal/home/navBar/Palestra.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,33 @@ namespace GymFitFinal.home.navBar
         public Palestra()
         {
             InitializeComponent();
-           
+            /*var htmlSource = new HtmlWebViewSource();
+
+
+            htmlSource.Html = @"<html>
+<haed>
+    <script type='text / javascript'
+            id = 'botcopy-embedder-d7lcfheammjct'
+            class='botcopy-embedder-d7lcfheammjct'
+            data-botId='5f638e4c06c5030008d4612d'
+    >
+        var s = document.createElement('script');
+        s.type = 'text/javascript'; s.async = true;
+        s.src = 'https://widget.botcopy.com/js/injection.js';
+        document.getElementById('botcopy-embedder-d7lcfheammjct').appendChild(s);
+    </script>
+</haed>
+<body>
+<h1>PROVA</h1>
+</body>
+</html>
+";
+            view.EvaluateJavaScriptAsync(htmlSource.Html);
+            view.Source = htmlSource;*/
+
+
+
+
         }
 
         private async void ToolbarItem_Clicked(object sender, EventArgs e)
